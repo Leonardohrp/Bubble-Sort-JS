78ffa1db3c94d790e5b9a2b1af64b0060eb8bf90
Leonardohrp <46636415+Leonardohrp@users.noreply.github.com>
2020-06-09 15:43:25 -0300

    feito

---
+++ index.js
@@ -0,0 +1,16 @@
+const arrayList = [5, 9, 4, 8, 2, 7, 1, 3, 6];
+
+console.log(bubbleSort(arrayList));
+
+function bubbleSort(array) {
+    for(i = 0; i < array.length - 1; i++) {
+        for(j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }   
+    }
+    return array;
+}
